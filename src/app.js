bb7339fa9c5a18143187de7bedb2f25f8ca1e173
guilhermealamo <guilhermealamo@hotmail.com>
2020-11-10 16:52:08 -0300

    O.O

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
     techs,
   } = request.body;
 
+
+
   const Repositorie =
   {
     id: uuid(),
@@ -31,16 +33,47 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: "0"
   }
+
+  if (!isUuid)
+    return response.json({ "error: ": "ID not found." })
+
   repositories.push(Repositorie);
   return response.json(Repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const reposIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (reposIndex < 0)
+    return response.json({ "error: ": "ID not found" })
+
+  const likes = repositories[reposIndex].likes;
+  console.log("tantoLikes: " + likes);
+
+  const nRepos = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+  repositories[reposIndex] = nRepos;
+  return response.json({ nRepos })
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const reposIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (reposIndex < 0)
+    return response.json({ "error: ": "ID not found" })
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
